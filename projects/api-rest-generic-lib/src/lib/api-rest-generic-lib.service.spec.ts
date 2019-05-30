3813aa1b0294d1e4f797ffdc138111508661b1b5
jerome <jeffyer38130@gmail.com>
2019-05-30 22:58:01 +0200

    Add tests

--- projects/api-rest-generic-lib/src/lib/api-rest-generic-lib.service.spec.ts
+++ projects/api-rest-generic-lib/src/lib/api-rest-generic-lib.service.spec.ts
@@ -1,6 +1,6 @@
 import {TestBed} from '@angular/core/testing';
 
-import {ApiRestGenericLibService} from './api-rest-generic-lib.service';
+import {ApiRestGenericLibService, SearchField} from './api-rest-generic-lib.service';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {ResponseApi} from './models/response-api';
 
@@ -143,6 +143,80 @@ describe('ApiRestGenericLibService', () => {
       expect(req.request.method).toEqual('POST');
 
       req.flush(mockTest);
+    });
+
+    it('patch', () => {
+
+      const {service} = setup();
+
+      const mockTest: TestData = {
+        name: 'Bob',
+        firstName: 'Anna',
+        id: 5
+      };
+
+      service.patch('http://localhost/data/5', mockTest)
+        .subscribe((data: TestData) => {
+          expect(data.name).toEqual(mockTest.name);
+        });
+
+      const req = httpTestingController.expectOne('http://localhost/data/5');
+
+      expect(req.request.method).toEqual('PATCH');
+
+      req.flush(mockTest);
+    });
+
+    it('delete', () => {
+
+      const {service} = setup();
+
+      service.delete('http://localhost/data/5')
+        .subscribe(data => {
+          expect(data).toBeNull();
+        });
+
+      const req = httpTestingController.expectOne('http://localhost/data/5');
+
+      expect(req.request.method).toEqual('DELETE');
+
+      req.flush(null);
+    });
+
+    it('search', () => {
+
+      const {service} = setup();
+
+      const mockTestData: TestData = {
+        name: 'Bob',
+        firstName: 'Anna',
+        id: 5
+      };
+
+      const mockResponseApi: ResponseApi<TestData> = {
+        count: 1,
+        next: null,
+        previous: null,
+        results: [
+          mockTestData
+        ]
+      };
+
+      const search: SearchField = {
+        name: 'Bob'
+      };
+
+      service.search(search)
+        .subscribe((data: ResponseApi<TestData>) => {
+          expect(data.results[0].name).toEqual(mockTestData.name);
+        });
+
+      const req = httpTestingController.expectOne(
+        'http://localhost/data?name=Bob');
+
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(mockResponseApi);
     });
   });
 });
