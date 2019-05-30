985c731132dfa9afad1bc60044f353876c519213
jerome <jeffyer38130@gmail.com>
2019-05-30 21:39:22 +0200

    Add tests and Travis

--- projects/api-rest-generic-lib/src/lib/api-rest-generic-lib.service.spec.ts
+++ projects/api-rest-generic-lib/src/lib/api-rest-generic-lib.service.spec.ts
@@ -1,12 +1,72 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ApiRestGenericLibService } from './api-rest-generic-lib.service';
+import {ApiRestGenericLibService} from './api-rest-generic-lib.service';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+
+class TestData {
+  name: string;
+  firstName: string;
+  id: number;
+
+}
 
 describe('ApiRestGenericLibService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: ApiRestGenericLibService = TestBed.get(ApiRestGenericLibService);
-    expect(service).toBeTruthy();
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+
+
+      TestBed.configureTestingModule({
+        providers: [ApiRestGenericLibService],
+        imports: [HttpClientTestingModule]
+      });
+
+      httpTestingController = TestBed.get(HttpTestingController);
+    }
+  );
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  describe(':', () => {
+
+    function setup() {
+      // We inject our service (which imports the HttpClient) and the Test Controller
+      let service: ApiRestGenericLibService<TestData>;
+      service = TestBed.get(ApiRestGenericLibService);
+      service.url = 'http://localhost/data';
+      return {service};
+    }
+
+
+    it('should be created', () => {
+
+      const {service} = setup();
+      expect(service).toBeTruthy();
+    });
+
+    it('getById', () => {
+
+      const {service} = setup();
+
+      const mockTest: TestData = {
+        name: 'Bob',
+        firstName: 'Anna',
+        id: 5
+      };
+
+      service.getById(5)
+        .subscribe((data: TestData) => {
+          expect(data.name).toEqual(mockTest.name);
+        });
+
+      const req = httpTestingController.expectOne('http://localhost/data/5');
+
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(mockTest);
+    });
   });
 });
